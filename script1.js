38b155100e006160af2f975ce38dae26bcfef2c6
rhrits <hritik.hritiksingh@gmail.com>
2024-10-15 23:40:01 +0530

    script 1

---
+++ script1.js
@@ -0,0 +1,10 @@
+
+document.querySelectorAll('.accordion-panel-module--expand-icon--knJ5Q').forEach(icon => {
+    // Use the parent element or dispatch a mouse event to simulate a click
+    const event = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
+    icon.dispatchEvent(event);  // Dispatch click event
+});
