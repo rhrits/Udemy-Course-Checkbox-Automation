07e4692843a37340d129eecf0c44bfe711802b38
rhrits <hritik.hritiksingh@gmail.com>
2024-10-15 23:42:28 +0530

    script 2

---
+++ script2.js
@@ -0,0 +1,10 @@
+
+document.querySelectorAll('.ud-fake-toggle-input.ud-fake-toggle-checkbox').forEach(checkbox => {
+    // Simulate a click event for each checkbox
+    const event = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
+    checkbox.dispatchEvent(event);  // Dispatch click event
+});
