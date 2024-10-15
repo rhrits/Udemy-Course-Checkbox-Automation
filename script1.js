f342fdfd0778f35b50b407a55cc313de7a9fb8b9
rhrits <hritik.hritiksingh@gmail.com>
2024-10-15 23:40:37 +0530

    comment

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-
+// Step 1: Expand all sections by clicking the expand icon
 document.querySelectorAll('.accordion-panel-module--expand-icon--knJ5Q').forEach(icon => {
     // Use the parent element or dispatch a mouse event to simulate a click
     const event = new MouseEvent('click', {
