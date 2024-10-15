8e7c35a038a6977df03c0742f0fd148274d5648f
rhrits <hritik.hritiksingh@gmail.com>
2024-10-15 23:42:55 +0530

    comment

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-
+// Step 2: Check all checkboxes by clicking the checkbox elements
 document.querySelectorAll('.ud-fake-toggle-input.ud-fake-toggle-checkbox').forEach(checkbox => {
     // Simulate a click event for each checkbox
     const event = new MouseEvent('click', {
